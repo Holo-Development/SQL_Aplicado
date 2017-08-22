778bfe2a448169a3b3b5c76546f61c95e7281c52
Angelo <an.candia@alumnos.duoc.cl>
2017-08-22 19:30:04 -0300

    Finalize datos proveedor y encargado venta

--- registro_datos.sql
+++ registro_datos.sql
@@ -1,119 +1,239 @@
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('15174255-6','','','','','','');
+VALUES('15174255-6','La desconozida','Direccion 1','(2)-22938483','Correo1@gmail.com','(56)-93857394','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('57772225-0','','','','','','');
+VALUES('57772225-0','La conozida','Los bambus','(2)-22374038','Correo2@gmail.com','(56)-99485859','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('23222118-6','','','','','','');
+VALUES('23222118-6','1000 y 1 producto','las maletas','(2)-22837303','Correo3@gmail.com','(56)-99484720','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('86846386-4','','','','','','');
+VALUES('86846386-4','Electrogar','avenida siempre viva 123','(2)-22837503','Correo4@gmail.com','(56)-90584759','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('78321865-8','','','','','','');
+VALUES('78321865-8','Pengeuin food','eeuu 234','(2)-2223849483','Correo5@gmail.com','(56)-995847730','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('64375437-1','','','','','','');
+VALUES('64375437-1','KKT','holywood 2384','(2)-2284750484','Correo6@gmail.com','(56)-90594837','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('64884963-K','','','','','','');
+VALUES('64884963-K','Aguas Andinas','vespucio sur 23874','(2)-229484734','Correo7@gmail.com','(56)-98475039','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('55717574-1','','','','','','');
+VALUES('55717574-1','Movistar','vespucio norte 12345','(2)-2284050383','Correo8@gmail.com','(56)-93628372','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('49658214-4','','','','','','');
+VALUES('49658214-4','Netflix','vespucio este 2345','(2)-2284848484','Correo9@gmail.com','(56)-916384738','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('54117374-9','','','','','','');
+VALUES('54117374-9','Windows Chile','vespucio oeste 234','(2)-2205739574','Correo10@gmail.com','(56)-92648504','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('66315831-7','','','','','','');
+VALUES('66315831-7','Ilimitada','vespucio centro','(2)-2205938474','Correo11@gmail.com','(56)-92648504','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('39115859-2','','','','','','');
+VALUES('39115859-2','SW Limitada','rotonda quilin 3e4','(2)-2294938383','Correo12@gmail.com','(56)-93847304','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('71563697-2','','','','','','');
+VALUES('71563697-2','HW Limitada','los silvestres','(2)-2204948505','Correo13@gmail.com','(56)-98379483','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('91874521-1','','','','','','');
+VALUES('91874521-1','Zafari 2052','el pino','(2)-2205058385','Correo14@gmail.com','(56)-9374837','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('14163475-5','','','','','','');
+VALUES('14163475-5','Nerfea Esto','el trebol 2345','(2)-2273649585','Correo15@gmail.com','(56)-924287328','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('47696193-9','','','','','','');
+VALUES('47696193-9','Soldados insumos','ñuñoa 2345','(2)-220593749','Correo16@gmail.com','(56)-93873874','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('15931819-2','','','','','','');
+VALUES('15931819-2','Alimentos A','providencia 345','(2)-2274639684','Correo17@gmail.com','(56)-943893489','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('98734835-6','','','','','','');
+VALUES('98734835-6','Alimentos B','antartica 23456','(2)-2284630693','Correo18@gmail.com','(56)-923904758','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('34258812-3','','','','','','');
+VALUES('34258812-3','Alimentos C','isla de pascua 3456','(2)-22948594','Correo19@gmail.com','(56)-9348948','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('66226157-2','','','','','','');
+VALUES('66226157-2','Alimentos ABC','san joaquin 23456','(2)-22747494','Correo20@gmail.com','(56)-939473','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('64963162-K','','','','','','');
+VALUES('64963162-K','Tecno niña','san joaquin 23456','(2)-22948475','Correo21@gmail.com','(56)-93947833','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('93432784-5','','','','','','');
+VALUES('93432784-5','Tecno Niño','san joaquin 234567','(2)-22847494','Correo22@gmail.com','(56)-9923837','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('87712735-4','','','','','','');
+VALUES('87712735-4','Storm object','mercurio','(2)-22958404','Correo23@gmail.com','(56)-99338245','0');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('87592348-K','','','','','','');
+VALUES('87592348-K','PC Factory','tulipanes 3784','(2)-22948474','Correo24@gmail.com','(56)-9983727','0');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('84461832-8','','','','','','');
+VALUES('84461832-8','EASY','flor del pajaro 345','(2)-22847595','Correo25@gmail.com','(56)-99398483','0');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('38127471-3','','','','','','');
+VALUES('38127471-3','Sodimac','benjamin 234567','(2)-22847494','Correo26@gmail.com','(56)-9938473','0');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('75483592-3','','','','','','');
+VALUES('75483592-3','Lider','providencia 334','(2)-22948474','Correo27@gmail.com','(56)-9893838','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('19441574-5','','','','','','');
+VALUES('19441574-5','Falabela','providencia 345','(2)-22948474','Correo28@gmail.com','(56)-9938473','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('35747757-3','','','','','','');
+VALUES('35747757-3','ZMART','tobalaba 345','(2)-22847575','Correo29@gmail.com','(56)-993837323','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('88988935-7','','','','','','');
+VALUES('88988935-7','Antartica Libros',' tobalaba 34','(2)-22718981','Correo30@gmail.com','(56)-9837262','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('33892721-5','','','','','','');
+VALUES('33892721-5','MC Donalds','tobalaba 34','(2)-22744981','Correo31@gmail.com','(56)-9938473','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('14574214-5','','','','','','');
+VALUES('14574214-5','Comida Verder','ahhhh','(2)-22648981','Correo32@gmail.com','(56)-98937327','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('63222291-2','','','','','','');
+VALUES('63222291-2','Comida Roja','EEUU 24','(2)-26518981','Correo33@gmail.com','(56)-9938473','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('92689115-4','','','','','','');
+VALUES('92689115-4','Licencias','Brazil 345','(2)-22734981','Correo34@gmail.com','(56)-9938473','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('12256892-K','','','','','','');
+VALUES('12256892-K','......','Chile 345','(2)-22373949','Correo35@gmail.com','(56)-993734763','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('98446964-0','','','','','','');
+VALUES('98446964-0','Paradiso','pedrocito','(2)-22938483','Correo36@gmail.com','(56)-9837362','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('56375898-8','','','','','','');
+VALUES('56375898-8','Paris','avenida brazil 34','(2)-22949383','Correo37@gmail.com','(56)-9837362','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('61249842-3','','','','','','');
+VALUES('61249842-3','Meteo red','sin ideas','(2)-22938304','Correo38@gmail.com','(56)-983736','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('71585565-8','','','','','','');
+VALUES('71585565-8','Nerf','git es la luz','(2)-22038394','Correo39@gmail.com','(56)-938737823','1');
 
 INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
-VALUES('31965863-7','','','','','','');
+VALUES('31965863-7','Bar de Moe','git hub tambien','(2)-22847520','Correo40@gmail.com','(56)-9938378','1');
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('6593924-k','Angelo Candia','correoEncargadoVenta1@gmail.com','(56)-9394847')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('6205056-k','Rolando Aburto','correoEncargadoVenta2@gmail.com','(56)-9384509')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('7966836-2','Benjamin Matamoros','correoEncargadoVenta3@gmail.com','(56)-9394857')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('24085847-9','Jose Contreras','correoEncargadoVenta4@gmail.com','(56)-946589367')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('10458280-k','Rafaella Candia','correoEncargadoVenta5@gmail.com','(56)-9837843')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('16553169-8','Koku','correoEncargadoVenta6@gmail.com','(56)-94378734')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('22387442-8','Oliver rios','correoEncargadoVenta7@gmail.com','(56)-9387483')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('20976902-6','Daniel Vergara','correoEncargadoVenta8@gmail.com','(56)-938938938')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('17926955-4','Francisco gallardo','correoEncargadoVenta9@gmail.com','(56)-93878347')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('8206474-5','Cindy Contador','correoEncargadoVenta10@gmail.com','(56)-987384783')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('20884039-8','Fabian Miranda','correoEncargadoVenta11@gmail.com','(56)-994739')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('17585123-2','Michelle no bachelet','correoEncargadoVenta12@gmail.com','(56)-98374878347')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('10186316-6','Pepe patea traseros','correoEncargadoVenta13@gmail.com','(56)-939849843')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('16515224-7','Ned Flander','correoEncargadoVenta14@gmail.com','(56)-99339849')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('22877207-0','Lisa simpson','correoEncargadoVenta15@gmail.com','(56)-9837837')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('16205575-5','Max Power','correoEncargadoVenta16@gmail.com','(56)-9387438')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('6300496-0','Max Power 2','correoEncargadoVenta17@gmail.com','(56)-9398943898')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('6698767-1','Oliver Aton','correoEncargadoVenta18@gmail.com','(56)-99389348')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('23474345-7','Jerry smith','correoEncargadoVenta19@gmail.com','(56)-9928398')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('16443983-6','Summer winter','correoEncargadoVenta20@gmail.com','(56)-985789547')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('10787727-4','iseufhsih','correoEncargadoVenta21@gmail.com','(56)-99784839')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('15953250-k','sfuhseifhsf','correoEncargadoVenta22@gmail.com','(56)-90389948')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('20597206-4','Vivanco Tapia','correoEncargadoVenta23@gmail.com','(56)-93874837')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('7408934-8','Arland Michellena','correoEncargadoVenta24@gmail.com','(56)-9938984')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('19607907-6','Tipo de abajao','correoEncargadoVenta25@gmail.com','(56)-998394839')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('5161089-k','Morgana flin','correoEncargadoVenta26@gmail.com','(56)-989879384')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('9568174-3','Futaba sakura','correoEncargadoVenta27@gmail.com','(56)-993849')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('16217783-4','Ann flores','correoEncargadoVenta28@gmail.com','(56)-93874837')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('10944674-2','Buho','correoEncargadoVenta29@gmail.com','(56)-926536253')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('18591719-3','Perro','correoEncargadoVenta30@gmail.com','(56)-983787')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('9887457-7','Gato','correoEncargadoVenta31@gmail.com','(56)-9376476736')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('18599036-2','Michael de Santa','correoEncargadoVenta32@gmail.com','(56)-987387348')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('22192193-3','CJ','correoEncargadoVenta33@gmail.com','(56)-97363342')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('15238495-5','Jesus','correoEncargadoVenta34@gmail.com','(56)-9376763')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('16808844-2','No jesus','correoEncargadoVenta35@gmail.com','(56)-9387438')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('15605131-4','Claudio contreras','correoEncargadoVenta36@gmail.com','(56)-9387483478')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('21320904-3','Miguel','correoEncargadoVenta37@gmail.com','(56)-9487834758')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('6991844-1','Tapia','correoEncargadoVenta38@gmail.com','(56)-9384787')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('16740949-0','Alexander Candia','correoEncargadoVenta39@gmail.com','(56)-98374837')
+
+INSERT INTO ENCARGADO_VENTA(runEncargado,nombreEncargado,correo,celular)
+VALUES('10001851-9','YO','correoEncargadoVenta40@gmail.com','(56)-98374837')
