87848ca9fe92cbad86ff5c35272c9a624c958aca
macpower <tanufuki@gmail.com>
2017-08-22 17:26:53 -0300

    Subi proveedores

---
+++ registro_datos.sql
@@ -0,0 +1,119 @@
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('15174255-6','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('57772225-0','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('23222118-6','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('86846386-4','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('78321865-8','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('64375437-1','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('64884963-K','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('55717574-1','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('49658214-4','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('54117374-9','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('66315831-7','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('39115859-2','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('71563697-2','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('91874521-1','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('14163475-5','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('47696193-9','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('15931819-2','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('98734835-6','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('34258812-3','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('66226157-2','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('64963162-K','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('93432784-5','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('87712735-4','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('87592348-K','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('84461832-8','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('38127471-3','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('75483592-3','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('19441574-5','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('35747757-3','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('88988935-7','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('33892721-5','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('14574214-5','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('63222291-2','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('92689115-4','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('12256892-K','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('98446964-0','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('56375898-8','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('61249842-3','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('71585565-8','','','','','','');
+
+INSERT INTO PROVEEDOR(runProveedor,nombreProveedor,direccion,telefono,correo,celular,esCliente)
+VALUES('31965863-7','','','','','','');
